7cef466fc0b133f2de3991b25b13ff48777cfc17
AnthonyMadia <aamadia24@gmail.com>
2023-01-03 12:12:34 -0500

    add: membership logic

--- Groups.sol
+++ Groups.sol
@@ -1,3 +1,4 @@
+// SPDX-License-Identifier: GPL-3.0
 pragma solidity ^0.7.0;
 
 import { Unirep } from '@unirep/contracts/Unirep.sol';
@@ -35,17 +36,17 @@ contract GroupManagement {
     function removeMember(string memory groupName, address member) public {
         require(groups[groupName].length > 0, "Group does not exist");
         require(isMember(groupName, member), "Member is not in group");
-        uint256 index = groups[groupName].indexOf(member);
-        // Remove the member from the group's member list
+        uint256 index = getMember(groupName, member);
+       // Remove the member from the group's member list
         delete groups[groupName][index];
         // Remove the group from the member's membership list
-        index = memberships[member].indexOf(groupName);
+        index = getMembership(member, groupName);
         delete memberships[member][index];
     }
 
     // Returns true if the given address is a member of the group
     function isMember(string memory groupName, address member) public view returns (bool) {
-        return groups[groupName].indexOf(member) != -1;
+        return getMember(groupName, member) != uint256(-1);
     }
 
     // Returns the total number of members in the group
@@ -56,5 +57,25 @@ contract GroupManagement {
     // Returns the list of groups that the given address is a member of
     function getMemberships(address member) public view returns (string[] memory) {
         return memberships[member];
+    }
+
+    // Helper function to search through the members of a group and return the index of the given member
+    function getMember(string memory groupName, address member) private view returns (uint256) {
+        for (uint256 i = 0; i < groups[groupName].length; i++) {
+            if (groups[groupName][i] == member) {
+                return i;
+            }
+        }
+        return uint256(-1);
+    }
+
+    // Helper function to search through the memberships of a member and return the index of the given group
+    function getMembership(address member, string memory groupName) private view returns (uint256) {
+        for (uint256 i = 0; i < memberships[member].length; i++) {
+            if (memberships[member][i] == groupName) {
+                return i;
+            }
+        }
+        return uint256(-1);
     }
 }
